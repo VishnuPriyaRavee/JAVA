65ad6d93191bdfc9a14e500647cb9486f9928b1f
VishnuPriyaRavee <VishnuPriyaRavee@users.noreply.github.com>
2016-07-19 16:48:17 +0530

    Create OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,15 @@
+class OddEven
+{
+public static void mnain(STring args[])
+{
+int a=Integer.parseInt(args[0]);
+if(a%2==0)
+{
+System.out.println("The number is"+a"is even");
+}
+else
+{
+System.out.println("The number"+a+"is odd");
+}
+}
+}
