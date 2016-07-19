2233f9e15715bf3b7a7d5da0e38e8d390d9c0dd1
VishnuPriyaRavee <VishnuPriyaRavee@users.noreply.github.com>
2016-07-19 17:01:22 +0530

    Create ReversingString.java

---
+++ ReversingString.java
@@ -0,0 +1,9 @@
+class ReversingString
+{
+   public static void main(String args[])
+   {
+	   String a="";
+      StringBuffer sb= new StringBuffer(a);
+      System.out.println(sb.reverse());
+   }
+}
