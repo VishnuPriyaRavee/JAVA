d822e0ccd1f32e0f4008032cb01cc81891845994
VishnuPriyaRavee <VishnuPriyaRavee@users.noreply.github.com>
2016-07-20 10:36:19 +0530

    Create Hcf.java

---
+++ Hcf.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Hcf
+{
+    public static void main(String args[])
+    {
+        int a, b, x, y, t, hcf, lcm;
+        Scanner scan = new Scanner(System.in);
+		
+        System.out.print("Enter Two Number : ");
+        x = scan.nextInt();
+        y = scan.nextInt();
+		
+        a = x;
+        b = y;
+		
+        while(b != 0)
+        {
+            t = b;
+            b = a%b;
+            a = t;
+        }
+		
+       
+        
+		
+        System.out.print("HCF = " +a);
+      
+    }
+}
