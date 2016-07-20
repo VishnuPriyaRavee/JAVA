34028ffbf01087b526a59481ba8d6bdba7a3dc06
VishnuPriyaRavee <VishnuPriyaRavee@users.noreply.github.com>
2016-07-20 17:33:51 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,13 @@
+class Swap
+{
+public static void main(String args[])
+{
+int temp;
+int a=Integer.parseInt(arhs[0])
+int b=Integer.parseInt(arhs[1])
+temp=b;
+a=b;
+b=temp;
+System.out.println(a+"  "+b);
+}
+}
