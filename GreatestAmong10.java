ae226f5cfb952ee6ed4cfed46939537af733e815
VishnuPriyaRavee <VishnuPriyaRavee@users.noreply.github.com>
2016-07-23 10:20:23 +0530

    Create GreatestAmong10.java

---
+++ GreatestAmong10.java
@@ -0,0 +1,22 @@
+class GreatestAmong10
+{
+public static void main(String[] args) {
+	
+Scanner s=new Scanner(System.in);
+int[] a=new int[10];
+for(int i=0;i<10;i++){
+a[i]=s.nextInt();
+}
+int max;
+a[0]=max;
+for(int j-0;j=a.length;j++)
+{
+if(a[j]>max)
+{
+a[j]=max;
+}
+}
+System.out.println(max);
+}
+}
+}
