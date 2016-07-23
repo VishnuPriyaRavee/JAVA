2b0602f1313e69bfd42a7fbba869bd6f10586bb8
VishnuPriyaRavee <VishnuPriyaRavee@users.noreply.github.com>
2016-07-23 10:15:47 +0530

    Create Greatest.java

---
+++ Greatest.java
@@ -0,0 +1,16 @@
+class Greatest
+{
+public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+int b=s.nextInt();
+int c=s.nextInt();
+if ( a > b && a > c )
+         System.out.println("First number is largest.");
+      else if ( b > a && b > c )
+         System.out.println("Second number is largest.");
+      else ( c > a && c > b )
+         System.out.println("Third number is largest.");
+         }
+         }
+      
